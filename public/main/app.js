d4453b4a8d20f40671b84e457f22b947c8503007
Robert K. Bell <robert.k.bell@gmail.com>
2015-12-06 14:57:12 +1100

    get markdown stream, using Ramda & Bacon

--- public/main/app.js
+++ public/main/app.js
@@ -1,19 +1,44 @@
+"use strict";
 
 import bootstrap from 'bootstrap';
 import 'bootstrap/css/bootstrap.css!'; // TODO: switch to less or sass for mixins & suchlike
-import md from 'markdown-it';
+import markdownit from 'markdown-it';
 import bacon from 'baconjs';
+import R from 'ramda';
 import $ from 'jquery';
-import he from 'he';
 
-var up   = $('#up').asEventStream('click');
-var down = $('#down').asEventStream('click');
 
-var counter =
-        // map up to 1, down to -1
-        up.map(1).merge(down.map(-1))
-            // accumulate sum
-            .scan(0, function(x,y) { return x + y });
+const elems = {
+    markdownInput: '#md-in',
+    htmlOut:       '#html-out'
+};
 
-// assign observable value to jQuery property text
-counter.assign($('#counter'), 'text');
+const md = markdownit();
+
+const logtap = R.curry((name = 'tap:', thing) => {
+    console.log(name, thing)
+});
+
+const markdownIn = $(elems.markdownInput)
+    .asEventStream('keyup change');
+
+
+const inputElementMarkdownToHtml = R.compose(
+    R.bind(md.render, md),
+    R.tap(logtap('original value:')),
+    R.pathOr('', ['value']),
+    R.tap(logtap('before pathOr'))
+);
+
+
+const eventToHtml = R.compose(
+    inputElementMarkdownToHtml,
+    R.path(['currentTarget'])
+);
+
+
+const htmlStream = markdownIn
+    .map(eventToHtml)
+    .startWith(inputElementMarkdownToHtml($(elems.markdownInput)));
+
+htmlStream.assign($(elems.htmlOut), 'text');
