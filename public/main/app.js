286cebd89518ba7c257e8000d8f3252403ca8002
Robert K. Bell <robert.k.bell@gmail.com>
2015-12-18 20:08:30 +1100

    tidy up appearance

--- public/main/app.js
+++ public/main/app.js
@@ -2,13 +2,14 @@
 
 import bootstrap from 'bootstrap';
 import 'bootstrap/css/bootstrap.css!'; // TODO: switch to less or sass for mixins & suchlike
+import 'style/app.css!';
 import markdownit from 'markdown-it';
 import Cycle from '@cycle/core';
 import {h, makeDOMDriver} from '@cycle/dom';
 import R from 'ramda';
 
 import hh from 'hyperscript-helpers';
-const {div, pre, code, label, textarea} = hh(h);
+const {div, a, pre, code, label, textarea} = hh(h);
 
 const elems = {
     markdownInput: '#md-in',
@@ -16,28 +17,43 @@ const elems = {
     appRoot:       '#app'
 };
 
+const initialInput = '# foo \nbar';
+
 const md = markdownit();
+
+const renderIOPageDom = (htmlOutput) =>
+    div('.panes__outer', [
+        div('.pane__input', [
+            label('.pane__heading', {
+                attributes: {
+                    for: '#md-in'
+                }
+            }, 'Markdown Input'),
+            textarea('#md-in.pane__text', {
+                attributes: {
+                    autofocus: 'autofocus'
+                }
+            })
+        ]),
+        div('.pane__output', [
+            div('.pane__heading', 'Output:'),
+            textarea('.pane__text', {
+                    attributes: {
+                        readonly: 'readonly'
+                    }
+                },
+                htmlOutput
+            )
+        ])
+    ]);
 
 function main(responses) {
     const requests = {
         DOM: responses.DOM.select(elems.markdownInput).events('input')
                  .map(ev => ev.target.value)
                  .map(R.bind(md.render, md))
-                 .startWith('')
-                 .map(htmlOutput =>
-                     div([
-                         div([
-                             label('Markdown Input'),
-                             textarea('#md-in')
-                         ]),
-                         div([
-                             div('Output:'),
-                             pre([
-                                 code(htmlOutput)
-                             ])
-                         ])
-                     ])
-                 )
+                 .startWith(initialInput)
+                 .map(renderIOPageDom)
     };
     return requests;
 }
