2e94b61d91ceb024f296d7dbecc4a4530e974d7e
Robert K. Bell <robert.k.bell@gmail.com>
2015-12-20 08:42:35 +1100

    add editorconfig, jscs

--- public/main/app.js
+++ public/main/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 import bootstrap from 'bootstrap';
 import 'bootstrap/css/bootstrap.css!'; // TODO: switch to less or sass for mixins & suchlike
@@ -13,77 +13,77 @@ import hh from 'hyperscript-helpers';
 const {div, a, pre, code, label, textarea} = hh(h);
 
 const elems = {
-    markdownInput: '#md-in',
-    htmlOut:       '#html-out',
-    appRoot:       '#app'
+  markdownInput: '#md-in',
+  htmlOut:       '#html-out',
+  appRoot:       '#app'
 };
 
-const initialInput = '# foo \nbar';
+const initialInput = 'foo\n\nbar\n\nbaz';
 
 const md = markdownit();
 
 const showMe = R.curry((label = 'showMe...', data) => {
-    console.group(label);
-    console.log(data);
-    console.groupEnd();
+  console.group(label);
+  console.log(data);
+  console.groupEnd();
 });
 
 const zipStreams = (inputStream, outputStream) => R.zipObj(
-    ['in', 'out'],
-    [inputStream, outputStream]
+  ['in', 'out'],
+  [inputStream, outputStream]
 );
 
 const renderIOPageDom = (inOut) =>
-    div('.panes__outer', [
-        div('.pane__input', [
-            label('.pane__heading', {
-                    attributes: {
-                        for: '#md-in'
-                    }
-                },
-                'Markdown Input'
-            ),
-            textarea('#md-in.pane__text', {
-                    attributes: {
-                        autofocus: 'autofocus'
-                    }
-                },
-                inOut.in
-            )
-        ]),
-        div('.pane__output', [
-            div('.pane__heading', 'Output:'),
-            textarea('.pane__text', {
-                    attributes: {
-                        readonly: 'readonly'
-                    }
-                },
-                inOut.out
-            )
-        ])
-    ]);
+  div('.panes__outer', [
+    div('.pane__input', [
+      label('.pane__heading', {
+          attributes: {
+            for: '#md-in'
+          }
+        },
+        'Markdown Input'
+      ),
+      textarea('#md-in.pane__text', {
+          attributes: {
+            autofocus: 'autofocus'
+          }
+        },
+        inOut.in
+      )
+    ]),
+    div('.pane__output', [
+      div('.pane__heading', 'Output:'),
+      textarea('.pane__text', {
+          attributes: {
+            readonly: 'readonly'
+          }
+        },
+        inOut.out
+      )
+    ])
+  ]);
 
 function main(responses) {
-    const inputStream = responses.DOM.select(elems.markdownInput)
-        .events('input')
-        .map(ev => ev.target.value)
-        .startWith(initialInput);
+  const inputStream = responses.DOM.select(elems.markdownInput)
+    .events('input')
+    .map(ev => ev.target.value)
+    .startWith(initialInput);
 
-    const outputStream = inputStream
-        .map(R.bind(md.render, md));
+  const outputStream = inputStream
+    .map(R.bind(md.render, md));
 
-    const bothStream = Rx.Observable.zip(
-        inputStream,
-        outputStream,
-        zipStreams
-    );
+  const bothStream = Rx.Observable.zip(
+    inputStream,
+    outputStream,
+    zipStreams
+  );
 
-    const requests = {
-        DOM: bothStream.map(renderIOPageDom)
-    };
-    return requests;
+  const requests = {
+    DOM: bothStream.map(renderIOPageDom)
+  };
+  return requests;
 }
 
 Cycle.run(main, {
-    DOM: makeDOMDriver(elems.appRoot)
+  DOM: makeDOMDriver(elems.appRoot)
 });
